236caeb0768ae791e980e7cee53c00499ab08fa9
DanielPrado0245455 <0245455@up.edu.mx>
2023-08-30 13:20:45 -0600

    New

--- src/Reportes.jsx
+++ src/Reportes.jsx
@@ -4,6 +4,15 @@ import LogoUp from "./img/LogoUniversidad.png";
 import ImgCaracter from "./img/inalterable 2.png";
 import Medio from "./img/Medio.png";
 import Conocimiento from "./img/Vector.png";
+import Material from "./img/Vector (1).png";
+import Arte from "./img/Vector (2).png";
+import Ayuda from "./img/Vector (3).png";
+import Status from "./img/license.png";
+import Bajo from "./img/Baja.png";
+import Normas from "./img/checkbox-multiple-marked-circle-outline.png";
+import Salud from "./img/heart-multiple-outline.png";
+import Alta from "./img/Alta.png";
+import ImgCaracter2 from "./img/reflexivo-teorico 1.png";
 
 function Reportes() {
   return (
@@ -63,6 +72,7 @@ function Reportes() {
           
           <div className="ValoresAlumno-top">
 
+            {/* Cuadro de valores del conocimiento */}
             <div className="ValoresAlumno-top-conocimiento">
               <div className="Top-conocimiento-top">
                 <div className="Conocimiento-titulo-general">
@@ -79,15 +89,130 @@ function Reportes() {
               <p className="Top-conocimiento-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
             </div>
 
-            <div className="ValoresAlumno-top-material"></div>
-            <div className="ValoresAlumno-top-arte"></div>
-            <div className="ValoresAlumno-top-ayuda"></div>
+            {/* Cuadro de valores del material */}
+            <div className="ValoresAlumno-top-material">
+              <div className="Top-material-top">
+                <div className="Material-titulo-general">
+                  <div className="Material-titulo-imgGeneral">
+                    <img src={Material} alt="" className="Material-titulo-img" />
+                  </div>
+                  <p className="Material-titulo">Sentido Practico Material</p>
+                </div>
+                <div className="Material-quantity-general">
+                  <img src={Medio} alt="" className="Material-grafic"/>
+                  <p className="Material-quantity">Medio</p>
+                </div>
+              </div>
+              <p className="Top-material-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
+
+            {/* Cuadro de valores del arte */}
+            <div className="ValoresAlumno-top-arte">
+              <div className="Top-arte-top">
+                <div className="Arte-titulo-general">
+                  <div className="Arte-titulo-imgGeneral">
+                    <img src={Arte} alt="" className="Arte-titulo-img" />
+                  </div>
+                  <p className="Arte-titulo">La Cultura y El Arte</p>
+                </div>
+                <div className="Arte-quantity-general">
+                  <img src={Medio} alt="" className="Arte-grafic"/>
+                  <p className="Arte-quantity">Medio</p>
+                </div>
+              </div>
+              <p className="Top-arte-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
+
+            {/* Cuadro de valores del ayuda */}
+            <div className="ValoresAlumno-top-ayuda">
+              <div className="Top-ayuda-top">
+                <div className="Ayuda-titulo-general">
+                  <div className="Ayuda-titulo-imgGeneral">
+                    <img src={Ayuda} alt="" className="Ayuda-titulo-img" />
+                  </div>
+                  <p className="Ayuda-titulo">Ayuda a Otros</p>
+                </div>
+                <div className="Ayuda-quantity-general">
+                  <img src={Medio} alt="" className="Ayuda-grafic"/>
+                  <p className="Ayuda-quantity">Medio</p>
+                </div>
+              </div>
+              <p className="Top-ayuda-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
           </div>
 
           <div className="ValoresAlumno-bottom">
-            <div className="ValoresAlumno-bottom-status"></div>
-            <div className="ValoresAlumno-bottom-calidad"></div>
-            <div className="ValoresAlumno-bottom-salud"></div>
+            <div className="ValoresAlumno-bottom-status">
+              <div className="Bottom-status-top">
+                  <div className="Status-titulo-general">
+                    <img src={Status} alt="" className="Status-titulo-img" />
+                    <p className="Status-titulo">Status y Reconocimiento</p>
+                  </div>
+                  <div className="Status-quantity-general">
+                    <img src={Bajo} alt="" className="Status-grafic"/>
+                    <p className="Status-quantity">Bajo</p>
+                  </div>
+              </div>
+              <p className="Top-Status-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
+
+
+            <div className="ValoresAlumno-bottom-calidad">
+              <div className="Top-calidad-top">
+                  <div className="Calidad-titulo-general">
+                    <img src={Normas} alt="" className="Calidad-titulo-img" />
+                    <p className="Calidad-titulo">Normas y Calidad</p>
+                  </div>
+                  <div className="Calidad-quantity-general">
+                    <img src={Alta} alt="" className="Calidad-grafic"/>
+                    <p className="Calidad-quantity">Alto</p>
+                  </div>
+              </div>
+              <p className="Top-calidad-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
+
+
+            <div className="ValoresAlumno-bottom-salud">
+              <div className="Top-salud-top">
+                <div className="Salud-titulo-general">
+                  <img src={Salud} alt="" className="Salud-titulo-img" />
+                  <p className="Salud-titulo">Salud y Deporte</p>
+                </div>
+                <div className="Salud-quantity-general">
+                  <img src={Medio} alt="" className="Salud-grafic"/>
+                  <p className="Salud-quantity">Medio</p>
+                </div>
+              </div>
+              <p className="Top-salud-info">Preferencia por todo lo referente al conocimiento, la verdad de las cosas y la ciencia</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Cáracter y métodos de estudio */}
+        <div className="Caracter-Metodos-general">
+
+          {/* Cuadro de cáracter */}
+          <div className="Caracter2-general">
+            <div className="Caracter2-info-img">
+                <img src={ImgCaracter2} alt="" className="Caracter2-img" />
+                <div className="Caracter2-info">
+                  <p className="Caracter2-info-tipo">REFLEXIVO - TEÓRICO</p>
+                  <p className="Caracter2-info-descripcion">Tiende a la objetividad, a considerarlas experiencias y observarlas desde diferentes ángulos. Reúne datos y los analiza metódicamente antes de llegar</p>
+                </div>
+            </div>
+          </div>
+
+          {/* Cuadro de métodos */}
+          <div className="Metodos-general">
+
+            <h1 className="Metodos-titulo">Métodos de Estudio</h1>
+
+            <div className="Metodos-top">
+
+              <div className="Top-primero-general">
+                
+              </div>
+            </div>
           </div>
         </div>
       </div>
